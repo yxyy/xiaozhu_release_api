51ecd9e32dc12a06d39b5df6ad09825a2ddd4dcb
liuhuacong <Liuhuacong8>
2024-11-11 14:55:41 +0800

    推广项目

--- router/market.go
+++ router/market.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"xiaozhu/internal/handler/market/assets/principal"
 	"xiaozhu/internal/handler/market/assets/proxy_company"
+	"xiaozhu/internal/handler/market/assets/proxy_project"
 )
 
 func InitMarketRouter(r *gin.Engine) {
@@ -21,6 +22,12 @@ func InitMarketRouter(r *gin.Engine) {
 		assets.POST("/proxy-company/update", proxy_company.Update)
 		assets.POST("/proxy-company/list", proxy_company.List)
 		assets.GET("/proxy-company/list-all", proxy_company.ListAll)
+
+		// 代理项目
+		assets.POST("/proxy-project/create", proxy_project.Create)
+		assets.POST("/proxy-project/update", proxy_project.Update)
+		assets.POST("/proxy-project/list", proxy_project.List)
+		assets.GET("/proxy-project/list-all", proxy_project.ListAll)
 
 	}
 }
