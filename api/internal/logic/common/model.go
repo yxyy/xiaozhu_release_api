3988a0184fc717c4f5da6a21396cada88df62df9
liuhuacong <Liuhuacong8>
2024-11-25 17:02:51 +0800

    公共参数

--- api/internal/logic/common/model.go
+++ api/internal/logic/common/model.go
@@ -1,6 +1,7 @@
 package common
 
 type RequestForm struct {
+	UserId    int    `json:"user_id"`
 	RequestId string `json:"request_id"`
 	GameId    int    `json:"game_id" binding:"required,gt=0"`
 	CpCode    string `json:"cp_code"`
@@ -8,6 +9,7 @@ type RequestForm struct {
 	Version   string `json:"version" binding:"required"`
 	Os        string `json:"os" binding:"oneof=android ios"`
 	DeviceId  string `json:"device_id" binding:"required"`
+	Ip        string `json:"ip"`
 	Debug     string `json:"debug" `
 }
 
